91ba45b9bbc257640a044e84e0a5ad3a59814a3a
masaya <masaya@masaya-PC>
2016-05-06 22:33:31 +0900

    First commit

---
+++ src/main/java/bestprofit/xbrl_analyzer/App.java
@@ -0,0 +1,13 @@
+package bestprofit.xbrl_analyzer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
