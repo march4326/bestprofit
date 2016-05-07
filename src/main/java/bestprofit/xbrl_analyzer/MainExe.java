ed74bbd821fc76c35e76e198f802570e2753e95a
masaya <masaya@masaya-PC>
2016-05-07 16:17:43 +0900

    log4jの設定追加とmainクラスのクラス名変更

---
+++ src/main/java/bestprofit/xbrl_analyzer/MainExe.java
@@ -0,0 +1,18 @@
+package bestprofit.xbrl_analyzer;
+
+import org.apache.log4j.Logger;
+
+public class MainExe
+{
+	private static final Logger logger = Logger.getLogger(MainExe.class);
+
+	public static void main( String[] args )
+    {
+    	logger.info("***** XBRL analyzing Start *****");
+
+
+
+
+    	logger.info("***** XBRL analyzing End *****");
+    }
+}
